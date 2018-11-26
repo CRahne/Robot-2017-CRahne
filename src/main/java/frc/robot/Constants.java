79c89bade0fbc6222468f26c6a32daf1a2c3ffae
Cole Rahne <colerahne1@student.isd423.org>
2018-11-26 12:24:06 -0600

    Added Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class Constants {
+
+    // DRIVETRAIN SUBSYSTEM
+    public Integer DTLeftRearTalonInteger = 0;
+    public Integer DTRightFrontTalonInteger = 1;
+    public Integer DTLeftFrontTalonInteger = 2;
+    public Integer DTRightRearTalonInteger = 3;
+
+    // CLIMB MOTORS
+    public Integer ClimbMainMotorTalonNumber = 4;
+    public Integer ClimbAltMotorTalonNumber = 7;
+
+    // SHOOTER SUBSYSTEM
+    public Integer ShootKickerTalonInteger = 1;
+    public Integer ShootMotorTalonInteger = 6;
+
+    // GATE + SWEEPER
+    public Integer GateTalonInteger = 0;
+    public Integer SweepMotorTalonInteger = 5;
+
+
+}
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,12 +8,16 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import frc.robot.Constants;
 
 /**
  * The RobotMap class contains the virtual representations of the components so
  * we can program them
  */
 public class RobotMap {
+  public Constants c = new Constants();
+  
   // Climb motors
   public static Talon CLIMB_MOTOR;
   public static Talon CLIMB_ALT;
@@ -43,30 +47,31 @@ public class RobotMap {
    * the initialize where everything is created and added to the runtime window
    */
   public static void init() {
+    Constants c = new Constants();
     // Climb motors inits
-    CLIMB_MOTOR = new Talon(4);
-    //LiveWindow.addActuator("Climb", "Motor 1", CLIMB_MOTOR);
+    CLIMB_MOTOR = new Talon(c.ClimbMainMotorTalonNumber);
+    LiveWindow.addActuator("Climb", "Motor 1", CLIMB_MOTOR);
 
-    CLIMB_ALT = new Talon(7);
-    //LiveWindow.addActuator("Climb", "Motor 2", CLIMB_ALT);
+    CLIMB_ALT = new Talon(c.ClimbAltMotorTalonNumber);
+    LiveWindow.addActuator("Climb", "Motor 2", CLIMB_ALT);
 
     // Drivetrain motors
-    DT_LEFTFRONT = new Talon(2);
-    // LiveWindow.addActuator("DriveTrain", "DT_LEFTFRONT", DT_LEFTFRONT);
+    DT_LEFTFRONT = new Talon(c.DTLeftFrontTalonInteger);
+    LiveWindow.addActuator("DriveTrain", "DT_LEFTFRONT", DT_LEFTFRONT);
 
-    DT_RIGHTFRONT = new Talon(1);
-    // LiveWindow.addActuator("DriveTrain", "DT_RIGHTFRONT", DT_RIGHTFRONT);
+    DT_RIGHTFRONT = new Talon(c.DTRightFrontTalonInteger);
+    LiveWindow.addActuator("DriveTrain", "DT_RIGHTFRONT", DT_RIGHTFRONT);
 
-    DT_LEFTREAR = new Talon(0);
-    // LiveWindow.addActuator("DriveTrain", "DT_LEFTREAR", DT_LEFTREAR);
+    DT_LEFTREAR = new Talon(c.DTLeftRearTalonInteger);
+    LiveWindow.addActuator("DriveTrain", "DT_LEFTREAR", DT_LEFTREAR);
 
-    DT_RIGHTREAR = new Talon(3);
-    // LiveWindow.addActuator("DriveTrain", "DT_RIGHTREAER", DT_RIGHTREAR);
+    DT_RIGHTREAR = new Talon(c.DTRightRearTalonInteger);
+    LiveWindow.addActuator("DriveTrain", "DT_RIGHTREAER", DT_RIGHTREAR);
     // Drivetrain others
     DT_GYRO = new ADXRS450_Gyro();
     DT_GYRO.reset();
     DT_GYRO.calibrate();
-    // LiveWindow.addSensor("DriveTrain", "DT_GYRO", DT_GYRO);
+    LiveWindow.addSensor("DriveTrain", "DT_GYRO", DT_GYRO);
 
     DRIVETRAIN = new MecanumDrive(DT_LEFTFRONT, DT_LEFTREAR, DT_RIGHTFRONT, DT_RIGHTREAR);
 
@@ -76,24 +81,24 @@ public class RobotMap {
     GEAR_CAM.setResolution(160, 120);
     GEAR_CAM.setFPS(30);
 
-    GEAR_SWITCH = new DigitalInput(0);
-    // LiveWindow.addSensor("GEAR SWITCH", 0, GEAR_SWITCH);
+    GEAR_SWITCH = new DigitalInput(c.GearSwitchDigitalInput);
+    LiveWindow.addSensor("GEAR SWITCH", 0, GEAR_SWITCH);
 
     // Shooter Stuff
     SHOOT_CAM = CameraServer.getInstance().startAutomaticCapture("SHOOTER", 1);
     SHOOT_CAM.setBrightness(0);
     SHOOT_CAM.setResolution(160, 120);
 
-    SHOOT_KICKER = new Talon(1);
-    // LiveWindow.addActuator("Shooter", "Kicker", SHOOT_KICKER);
+    SHOOT_KICKER = new Talon(c.ShootKickerTalonInteger);
+    LiveWindow.addActuator("Shooter", "Kicker", SHOOT_KICKER);
 
-    SHOOT_MOTOR = new Talon(6);
-    // LiveWindow.addActuator("Shooter", "Motor", (Talon) SHOOT_MOTOR);
+    SHOOT_MOTOR = new Talon(c.ShootMotorTalonInteger);
+    LiveWindow.addActuator("Shooter", "Motor", (Talon) SHOOT_MOTOR);
 
-    GATE = new Talon(0);
-    // LiveWindow.addActuator("Shooter", "Gate", GATE);
+    GATE = new Talon(c.GateTalonInteger);
+    LiveWindow.addActuator("Shooter", "Gate", GATE);
 
-    SWEEP_MOTOR = new Talon(5);
-    // LiveWindow.addActuator("Sweeper", "Motor", (Talon) SWEEP_MOTOR);
+    SWEEP_MOTOR = new Talon(c.SweepMotorTalonInteger);
+    LiveWindow.addActuator("Sweeper", "Motor", (Talon) SWEEP_MOTOR);
   }
 }
